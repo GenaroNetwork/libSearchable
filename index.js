472966333696974b5af81f289772b024699eb236
孟子易 <mengziyi540841@gmail.com>
2019-09-21 21:56:14 +0800

    emmm

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-//'Cascadia Code', 文泉驿微米黑,Consolas, 'Courier New', monospace
+import { Readable } from 'stream';
+import { ReadStream, WriteStream } from 'fs';
 
 const { Environment } = require('libgenaro');
 const path = require("path")
+const fs = require("fs")
+const Wallet = require("ethereumjs-wallet")
+const cryptico = require("cryptico") 
 
 export class {
     constructor(config){
         this._env = new Environment(config)
+        this._config = config
         this._bucketId = null
         this._bucketFiles = []
+        this._wallet = Wallet.fromV3(config.keyFile, config.passphrase)
+        innerCache.config = config
     }
 
     listBucket(){
@@ -45,9 +52,49 @@ export class {
         throw new Error("We Do Not Support Copy File Right Now")
     }
 
-    createReadStream(path, ...args){}
+    createReadStream(path, ...args){
+        const that = this
+        let fileName = path.normalize(`~/.cache/${new Date().getTime()}`)
+        class readStream extends Readable{
+            constructor(){
+                super()
+                tis.rs = fs.createReadStream(fileName)
+            }
+            async _read(){
+                await that.readFile(path, fileName)
+                this.rs.on("data", data => {
+                    this.push(data)
+                })
+                this.rs.on("end", () => {
+                    this.push(null)
+                    fs.unlink(fileName)
+                })
+            }
 
-    createWriteStream(path, ...args){}
+        }
+        return new readStream
+    }
+
+    createWriteStream(path, ...args){
+        const that = this
+        let fileName = path.normalize(`~/.cache/${new Date().getTime()}`)
+        class writeStream extends WritableStream{
+            constructor(){
+                super()
+                this.ws = fs.createWriteStream(fileName)
+            }
+            _write(chunk, encoding, callback){
+                tis.ws.write(chunk, encoding, callback)
+            }
+            async _final(callback){
+                tis.ws.end()
+                await that.writeFile(path, fileName)
+                fs.unlink(fileName)
+                callback()
+            }
+        }
+        return new writeStream
+    }
 
     async state(path, ...args){
         if (path.startsWith("/")) path = path.substr(1)
@@ -62,13 +109,52 @@ export class {
     writeFile(path, filePath, fileName, ...args){
         if (path.startsWith("/")) path = path.substr(1)
         return new Promise((res, rej) => {
-            let filename = fs.get
+            const keyCtr = this._env.generateEncryptionInfo(this._bucketId)
+            const index = keyCtr.index
+            const key = keyCtr.key
+            const ctr = keyCtr.ctr
             let stats = this._env.storeFile(this._bucketId, localPath, true, {
-                filename: path.join(path, fileName)
+                filename: path.join(path, fileName),
+                index,
+                key,
+                ctr,
+                progressCallback(){},
+                finishedCallback(err){
+                    if(err) rej(err)
+                    res()
+                }
             })
         })
     }
 
-    async readFile(path, ...args){}
+    readFile(path, fileSavePath, ...args){
+        return new Promise((res,rej) => {
+            if (path.startsWith("/")) path = path.substr("/")
+            let file = this._bucketFiles.find(file => file.filename === path)
+            if (!file) throw new Error("No Such File")
+            let key = '';
+            let ctr = '';
+            if (file.rsaKey && file.rsaCtr) {
+              let decryptionKey = cryptico.decrypt(file.rsaKey, this._wallet.getPrivateKey());
+              let decryptionCtr = cryptico.decrypt(file.rsaCtr, this._wallet.getPrivateKey());
+              if (decryptionKey.plaintext && decryptionCtr.plaintext) {
+                key = decryptionKey.plaintext;
+                ctr = decryptionCtr.plaintext;
+              }
+            }
+            this._env.resolveFile(bucketId, file.id, filePath, {
+                overwrite: true,
+                progressCallback: (process, allBytes) => {
+                },
+                finishedCallback: (err, fileId) => {
+                  if (err) rej(err)
+                  else res(fileId)
+                },
+                key,
+                ctr,
+                decrypt: true
+              });
+        })
+    }
 
 }
--- test.js
+++ test.js
@@ -1,17 +1,8 @@
-// const libgenaro = new Environment({
-//     bridgeUrl: 'http://111.111.111.111:8080',
-//     keyFile: `{
-//         "address": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//         "crypto":{"ciphertext":"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-//         "cipherparams":{"iv":"cccccccccccccccccccccccccccccccc"},"cipher":"aes-128-ctr",
-//         "kdf":"scrypt","kdfparams":{"dklen":32,"salt":"dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd","n":262144,"r":8,
-//         "p":1},"mac":"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"},
-//         "id": "ffffffff-1111-2222-3333-444444444444",
-//         "version": 3
-//     }
-//     `,
-//     passphrase: '123456',
-// });
+const libgenaro = new Environment({
+    bridgeUrl: 'http://111.111.111.111:8080',
+    keyFile: `"version":3,"id":"8922125c-09ef-4281-9b4b-4d0100092440","address":"47fa7b263f5b97a964572cc91afb0f9a9b3a147c","crypto":{"ciphertext":"e7493eef01720079e32dc3b330624b075215d3de3ce7b069cd99a63ec6378b32","cipherparams":{"iv":"425b0220a635b19f369e8d84075359e5"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"bd8b5a65d51860a699949b975b2c00ff38b19eaf18a41ba4268bdc457f8ba527","n":262144,"r":8,"p":1},"mac":"dfd5069c74fdb43cb3c75d0a654887924bf471be9e7043daa58751029146734e"},"name":"邮件·附件"}`,
+    passphrase: '123456',
+});
 
 let path = require("path")
 
